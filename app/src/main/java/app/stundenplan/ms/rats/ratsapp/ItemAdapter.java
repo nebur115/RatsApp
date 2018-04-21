5fecfbd24c63cb7dc5c7948301bd81c1be61d55e
nebur115 <rubenfoerster@gmx.de>
2018-04-21 14:17:36 +0200

    Test

--- app/src/main/java/app/stundenplan/ms/rats/ratsapp/ItemAdapter.java
+++ app/src/main/java/app/stundenplan/ms/rats/ratsapp/ItemAdapter.java
@@ -1,15 +1,20 @@
 package app.stundenplan.ms.rats.ratsapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +25,6 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final static int TYPE_EREIGNIS=1,TYPE_Obtionen=2, TYPE_Datum=3;
     private List<Object> itemFeed = new ArrayList();
     private Context context;
-
 
 
 
@@ -148,16 +152,59 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
 
-    public static class obtionenViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class obtionenViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView editText;
-
+        private Switch myswitch;
 
 
         public obtionenViewHolder(View itemView) {
             super(itemView);
             editText = (EditText) itemView.findViewById(R.id.editText2);
+            myswitch = (Switch) itemView.findViewById(R.id.switch1);
+            final SharedPreferences settings = context.getSharedPreferences("RatsVertretungsPlanApp",0);
             itemView.setOnClickListener(this);
+            myswitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!myswitch.isChecked()){
+                        if(settings.contains("Stundenliste")){
+                            editText.setVisibility(View.GONE);
+                            //Reaload nur mit eigenen Stunden
+                        }
+                        else{
+
+                            Toast.makeText(context, "Du musst erst einen Stundenplan erstellen, um nur deine Stunden anzuzeigen", Toast.LENGTH_SHORT).show();
+                            myswitch.setChecked(true);
+                        }
+
+                    }
+                    else {
+                        editText.setVisibility(View.VISIBLE);
+                        //Reaload mit allen Stunden
+                    }
+                }
+            });
+
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    //Teste ob editText.getText einer exestierenden Stufe entspricht und wenn ja Reload mit der Stufe.
+                    //Die änderung darf nicht in die SharedPref. gehen.
+                }
+            });
+
+
         }
 
         public void onClick(View view){
@@ -165,7 +212,15 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             RelativeLayout ZeigObtionsConstraint = itemView.findViewById(R.id.ZeigOptionsConstraint);
             ZeigObtionsConstraint.setVisibility(View.VISIBLE);
             trigger.setVisibility(View.INVISIBLE);
+            SharedPreferences settings = context.getSharedPreferences("RatsVertretungsPlanApp",0);
+            myswitch.setChecked(!settings.contains("Stundenliste"));
+            if(settings.contains("Stundenliste")){
+                editText.setVisibility(View.GONE);
+            }
         }
+
+
+
 
         public void showObtionenDetails(Obtionen obtionen){
             String EditText = obtionen.getEditText();
